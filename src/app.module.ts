785b31b6885b95acfbb342202f1e838e077f02f6
Louis <louis.dumont@viacesi.fr>
2024-06-12 08:52:51 +0200

    moved id_restaurant to user table (so deleting users_restaurants table as it is useless now))

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 import { RestaurantsModule } from './restaurant/restaurant.module';
 import { ProductsModule } from './product/product.module';
 import { MenusModule } from './menu/menu.module';
-import { UsersRestaurantsModule } from './users_restaurants/users_restaurants.module';
 
 @Module({
-  imports: [
-    RestaurantsModule,
-    ProductsModule,
-    MenusModule,
-    UsersRestaurantsModule,
-  ],
+  imports: [RestaurantsModule, ProductsModule, MenusModule],
   controllers: [],
   providers: [],
 })
--- src/users_restaurants/dto/create-users_restaurant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateUsersRestaurantDto {
-  @ApiProperty({ default: '111111111111111111111111' })
-  id_restaurant: string;
-
-  @ApiProperty({ default: '111111111111111111111111' })
-  id_user: string;
-}
